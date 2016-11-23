4b7ae1160a92ac239820711c0d506f78cdd7785e
ALO10089 <amine.louati@viseo.com>
2016-11-24 00:25:13 +0100

    odata ==> Json

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -9,50 +9,84 @@ sap.ui.define([
 		onInit: function() {
 			var oViewModel,
 				fnSetAppNotBusy,
-				iYear  = 2008, 
+				iYear = 2008,
 				aYears = [],
 				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
 
-var aMonths=[ {id: 1, description:"Janvier"},
-			  {id: 2, description:"Février"},
-			  {id: 3, description:"Mars"},
-			  {id: 4, description:"Avril"},
-			  {id: 5, description:"Mai"},
-			  {id: 6, description:"Juin"},
-			  {id: 7, description:"Juillet"},
-			  {id: 8, description:"Août"},
-			  {id: 9, description:"Septembre"},
-			  {id: 10, description:"Octobre"},
-			  {id: 11, description:"Novembre"},
-			  {id: 12, description:"Décembre"}];
+			var aMonths = [{
+				id: 1,
+				description: "Janvier"
+			}, {
+				id: 2,
+				description: "Février"
+			}, {
+				id: 3,
+				description: "Mars"
+			}, {
+				id: 4,
+				description: "Avril"
+			}, {
+				id: 5,
+				description: "Mai"
+			}, {
+				id: 6,
+				description: "Juin"
+			}, {
+				id: 7,
+				description: "Juillet"
+			}, {
+				id: 8,
+				description: "Août"
+			}, {
+				id: 9,
+				description: "Septembre"
+			}, {
+				id: 10,
+				description: "Octobre"
+			}, {
+				id: 11,
+				description: "Novembre"
+			}, {
+				id: 12,
+				description: "Décembre"
+			}];
 
-for(var i = iYear; i <= new Date().getFullYear(); i++ ){
-	
-	aYears.push({id: i, description:i}  );
-	
-}
+			for (var i = iYear; i <= new Date().getFullYear(); i++) {
 
+				aYears.push({
+					id: i,
+					description: i
+				});
 
+			}
 
 			oViewModel = new JSONModel({
 				busy: true,
 				delay: 0,
 				months: aMonths,
-				years: aYears
+				years: aYears,
+				Affectations: []
 			});
 			this.setModel(oViewModel, "appView");
 
 			fnSetAppNotBusy = function() {
 				oViewModel.setProperty("/busy", false);
 				oViewModel.setProperty("/delay", iOriginalBusyDelay);
+				var oModel = this.getOwnerComponent().getModel();
+				oModel.read("/Affectations", {success: this._onSuccess.bind(this) })
 			};
 
 			this.getOwnerComponent().getModel().metadataLoaded().
-			then(fnSetAppNotBusy);
+			then(fnSetAppNotBusy.bind(this));
 			this.getOwnerComponent().getModel().setUseBatch(false);
 			// apply content density mode to root view
 			this.getView().addStyleClass("sapUiSizeCompact");
+		},
+		
+		_onSuccess: function(response){
+			this.getModel("appView").setProperty("/Affectations", response.results);
 		}
+		
 	});
 
 });
