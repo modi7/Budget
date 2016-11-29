8014af1f824a1557b9c833a67f9223d8e29ec483
Amine LOUATI <alouati@viseo.net>
2016-11-29 23:06:11 +0000

    upload File

--- webapp/controller/Upload.controller.js
+++ webapp/controller/Upload.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"budget/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/routing/History",
+	"sap/m/MessageBox"
+], function(Controller, JSONModel, History, MessageBox) {
 	"use strict";
 
 	return Controller.extend("budget.controller.Upload", {
@@ -10,9 +13,80 @@ sap.ui.define([
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf budget.view.Upload
 		 */
-		//	onInit: function() {
-		//
-		//	},
+		onInit: function() {
+			this.getRouter().getTargets().getTarget("upload").attachDisplay(null, this._onUpload, this);
+			var oViewModel = new JSONModel({
+				busy: false,
+				id: null,
+				month: null,
+				year: null
+			});
+			this.setModel(oViewModel, "uploadView");
+
+			this._reader = new FileReader();
+			this._reader.addEventListener("load", this.onReaderLoad.bind(this), false)
+
+		},
+
+		onCancel: function() {
+
+			MessageBox.confirm("Are you sur you want to Cancel?", {
+				//	styleClass: oComponent.getContentDensityClass(),
+				onClose: function(oAction) {
+					if (oAction === sap.m.MessageBox.Action.OK) {
+						//that.getModel("appView").setProperty("/addEnabled", true);
+						this.getModel().resetChanges();
+						this._navBack();
+					}
+				}.bind(this)
+			});
+
+		},
+
+		_navBack: function() {
+			/*			var sPreviousHash = History.getInstance().getPreviousHash();
+						if (sPreviousHash !== undefined) {
+							history.go(-1);
+						} else {*/
+			//this.getRouter().navTo("object", {}, true);
+			this.getRouter().getTargets().display("object");
+			// }
+		},
+
+		_onUpload: function(oEvent) {
+			var oData = oEvent.getParameter("data");
+			var oModel = this.getModel("uploadView");
+			oModel.setProperty("/id", oData.id);
+			oModel.setProperty("/month", oData.month);
+			oModel.setProperty("/year", oData.year);
+			oModel.setProperty("/busy", true);
+			this._reader.readAsText(oData.file);
+			/*			
+						var oContext = this.getModel().createEntry("Ecritures", {
+									success: this._fnEntityCreated.bind(this),
+									error: this._fnEntityCreationFailed.bind(this),
+							properties: {
+								CompteId: oData.id,
+								Month: oData.month,
+								Year: oData.year
+							}
+						});
+						this.getView().setBindingContext(oContext);
+			  */
+		},
+
+		onReaderLoad: function(oEvent) {
+			var test = "";
+			if (oEvent.returnValue) {
+				var sContents = oEvent.target.result;
+				var aContents = sContents.split("\r\n");
+			}
+
+			this.getModel("uploadView").setProperty("/busy", false);
+			//	var contents = event.target.result;
+			//	var myTable = contents.split("\n");
+
+		}
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
